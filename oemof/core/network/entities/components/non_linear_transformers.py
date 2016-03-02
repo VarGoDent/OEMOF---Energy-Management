3a2a0aeca32967fe9690eda9030c46d469976685
Elisa Papadis <Elisa.Papadis@rl-institut.de>
2016-03-02 18:26:12 +0100

    Components Simple and Storage adjusted to hierarchical model

---
+++ oemof/core/network/entities/components/non_linear_transformers.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+from .transformers import Storage
+from .transformers import Simple
+import logging
+import numpy as np
+
+
+class CustomizedSimple(Simple):
+    """
+    A customized Transformer with variable electrical efficiency
+    Note: The model uses constraints which require binary variables, hence
+    objects of this class will results in mixed-integer-linear-problems.
+    Represents a diesel generator
+
+    Parameters
+    ----------
+    eta_total : float
+        total constant efficiency for the transformer
+    eta_el : list
+        list containing the minimial (first element) and maximal (second
+        element) electrical efficiency (0 <= eta_el <= 1)
+    """
+    optimization_options = {}
+
+    def __init__(self, **kwargs):
+
+        super().__init__(**kwargs)
+
+        self.min_load = kwargs.get('min_load', 0.1)
+
+        self.eta_total = sum(self.eta)
+        self.min_loading = self.min_load / 100 * self.out_max
+        #self.power
+
+        ## calculate minimal
+        #self.in_min = [self.out_min[0] / self.eta_min[0]]
+        #self.in_max = [self.out_max[0] / self.eta[0]]
+
+        #A = np.array([[1, self.out_min[0]],
+                      #[1, self.out_max[0]]])
+        #b = np.array([self.in_min[0], self.in_max[0]])
+        #self.coeff = np.linalg.solve(A, b)
+
+    def power_generation(self, residual_load, **kwargs):
+        power_output = np.maximum(self.min_loading, residual_load)
+        power_surplus = power_output - residual_load
+        return power_output, power_surplus
+
+    def fuel_costs_and_volume_py(self, power_flow, **kwargs):
+        peak_load = np.diff(np.concatenate((np.array([0]), power_flow)))
+        peak_load = peak_load.clip(min=0, max=None)
+        base_load = power_flow - peak_load
+        fuel_volume = (base_load / self.eta_min * 100 + peak_load /
+                       self.eta * 100)
+        fuel_costs = fuel_volume.sum() * self._economics.fuel_costs()  # economics
+        return fuel_costs, fuel_volume
+
+    def maximum_output(self, t=None, year=None):
+        return self.power
+
+
+class CustomizedStorage(Storage):
+    """
+    Parameters
+    ----------
+    cycle_no : float
+        number of cycles of the battery
+    """
+    optimization_options = {}
+
+    def __init__(self, power=0, capacity=0, **kwargs):
+
+        super().__init__(**kwargs)
+
+        self.cycle_no = kwargs.get('cycle_no', 2000)
+        self.capex_po = kwargs.get('capex_po', 0)
+        self.capex_ca = kwargs.get('capex_ca', 0)
+        self.dod_max = kwargs.get('dod_max', 0.80)
+        # parent class component holds lifetime
+
+        self.__current_soc = self.cap_initial
+
+        self.__power_ch = power * self.eta_in
+        self.__power_dch = power * self.eta_out
+        self.__capacity = capacity
+        self.__current_soc = self.cap_initial
+
+    def maximum_output(self, t=None, year=None):
+        return self.maximum_discharge()
+
+    def maximum_discharge(self):
+        return np.maximum(0, np.minimum(self.__power_dch,
+            (self.__current_soc - (1 - self.dod_max)) *
+            self.__capacity) * self.eta_out)
+
+    def maximum_charge(self):
+        return np.minimum(self.__power_ch, (1 - self.__current_soc) *
+            self.__capacity / self.eta_in)
+
+    def discharge(self, residual_load):
+        power_dch = np.minimum(residual_load, self.maximum_discharge())
+        self.__current_soc = (self.__current_soc - power_dch /
+            self.__capacity / self.eta_out)
+        return power_dch, self.__current_soc
+
+    def charge(self, residual_load):
+        power_ch = np.minimum(residual_load, self.maximum_charge())
+        self.__current_soc = (self.__current_soc + power_ch /
+            self.__capacity / self.eta_in)
+        return power_ch, self.__current_soc
+
+    #def self_discharge(self):
+        #self.__current_soc = np.maximum(0, self.__current_soc *
+            #(1 - self.__self_dch))
