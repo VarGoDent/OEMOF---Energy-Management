f70f057541cbb58b3c778b6ec3aa66ca0b9ae517
Caroline Moeller <caroline.moeller@rl-institut.de>
2016-05-04 19:02:58 +0200

    add soc_max and soc_min to charge and discharge limit rule

--- oemof/solph/linear_constraints.py
+++ oemof/solph/linear_constraints.py
@@ -652,11 +652,13 @@ def add_storage_charge_discharge_limits(model, block):
     c_rate_out = {obj.uid: obj.c_rate_out for obj in block.objs}
     c_rate_in = {obj.uid: obj.c_rate_in for obj in block.objs}
     cap_max = {obj.uid: obj.cap_max for obj in block.objs}
+    soc_max = {obj.uid: obj.soc_max for obj in block.objs}
+    soc_min = {obj.uid: obj.soc_min for obj in block.objs}
 
     def storage_discharge_limit_rule(block, e, t):
         expr = 0
         expr += model.w[e, model.O[e][0], t]
-        expr += -(cap_max[e] + block.add_cap[e]) \
+        expr += -(cap_max[e] + block.add_cap[e]) * (soc_max[e] - soc_min[e]) \
             * c_rate_out[e]
         return(expr <= 0)
     block.discharge_limit_invest = po.Constraint(block.uids,
@@ -667,7 +669,7 @@ def add_storage_charge_discharge_limits(model, block):
     def storage_charge_limit_rule(block, e, t):
         expr = 0
         expr += model.w[e, model.I[e][0], t]
-        expr += -(cap_max[e] + block.add_cap[e]) \
+        expr += -(cap_max[e] + block.add_cap[e] * (soc_max[e] - soc_min[e])) \
             * c_rate_in[e]
         return(expr <= 0)
     block.charge_limit_invest = po.Constraint(block.uids,
