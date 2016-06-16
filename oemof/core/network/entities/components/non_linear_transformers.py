cc925837d2c80557d8867d53b032a2c8a9785c92
Hendrik <hendrik.huyskens@rl-institut.de>
2016-06-16 14:23:32 +0200

    Added PV, changed power gen

--- oemof/core/network/entities/components/non_linear_transformers.py
+++ oemof/core/network/entities/components/non_linear_transformers.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-from .transformers import Storage
-from .transformers import Simple
+from .transformers import Storage, Simple
+from .sources import FixedSource
 import numpy as np
 from scipy.interpolate import interp1d
 
@@ -33,23 +33,20 @@ class CustomizedSimple(Simple):
         self.eta_total = self.eta
         self.min_loading = self.min_load * self.out_max
 
-    def power_generation(self, residual_load, **kwargs):
+    def power_generation(self, residual_load, RM_needed=0.0):
         """
         Define power generation value of generator while considering the
         case that the generator can be switched off (power = 0).
         """
-        power_output = 0.0
-        power_surplus = 0.0
-        if residual_load == 0:
-            power_output = 0.0
-            power_surplus = 0.0
-        else:
-            power_output = np.maximum(self.min_loading, residual_load)
+        power_needed = np.maximum(residual_load, RM_needed)
+        if power_needed > 0.0:
+            power_output = np.maximum(self.min_loading, power_needed)
             power_surplus = power_output - residual_load
+            return power_output, power_surplus
+        else:
+            return 0.0, 0.0
 
-        return power_output, power_surplus
-
-    def fuel_and_volume(self, power_flow, diesel_price, **kwargs):
+    def fuel_volume(self, power_flow, **kwargs):
         """
         Calculate fuel costs and fuel volume according to power flow
         and technical and financial input parameters.
@@ -62,7 +59,7 @@ class CustomizedSimple(Simple):
             price of diesel at specific location
         """
         eff = np.array([0, self.eta_min, self.eta_total])
-        load = np.array([0, self.out_max * self.eta_min / 100, self.out_max])
+        load = np.array([0, self.min_loading, self.out_max])
         interp = interp1d(load, eff)
 
         # Show efficiency curve:
@@ -76,9 +73,8 @@ class CustomizedSimple(Simple):
         energyThermic = power_flow[effOutput > 0].div(
             effOutput[effOutput > 0] / 100).sum()
         fuelVolume = energyThermic / HEAT_VALUE
-        fuelCosts = fuelVolume * diesel_price
 
-        return fuelCosts, fuelVolume
+        return fuelVolume
 
     def maximum_output(self, t=None, year=None):
         return self.power
@@ -99,7 +95,6 @@ class CustomizedStorage(Storage):
 
         self.capacity = kwargs.get('capacity', 0)
         self.crate = kwargs.get('c_rate_in', 1)
-        self.power = kwargs.get('power', 0)
         self.power = self.capacity * self.crate  # kW
 
         self.cycle_no = kwargs.get('cycle_no', 2000)
@@ -111,40 +106,64 @@ class CustomizedStorage(Storage):
 
         self.__power_ch = self.power / self.eta_in
         self.__power_dch = self.power
-        self.__capacity = self.capacity
+
+    def changeSize(self, size):
+        self.capacity = size
+        self.power = size * self.crate
+        self.__current_soc = self.cap_initial
+
+        self.__power_ch = self.power / self.eta_in
+        self.__power_dch = self.power
 
     def maximum_output(self):
         return self.maximum_discharge_el()
 
     def maximum_discharge_el(self):
-        return np.maximum(0.0, np.minimum(self.__power_dch, (
-            self.__current_soc - 1 + self.dod_max) * (
-                self.__capacity * self.eta_out)))
+        return np.maximum(0.0, np.minimum(
+            self.__power_dch,
+            (self.__current_soc - 1 + self.dod_max) *
+            (self.capacity * self.eta_out)))
 
     def maximum_charge_el(self):
-        return np.minimum(self.__power_ch,
-                  (1.0 - self.__current_soc) * self.__capacity / self.eta_in)
+        return np.minimum(
+            self.__power_ch,
+            (1.0 - self.__current_soc) * self.capacity / self.eta_in)
 
     def discharge(self, residual_load):
-        power_dch = np.minimum(residual_load, self.maximum_discharge_el())
-        energy_dch = np.minimum((power_dch / self.eta_out), (
-            self.__current_soc - 1 + self.dod_max) * self.__capacity)
+        if self.capacity == 0:
+            return 0.0, 0.0
+        else:
+            power_dch = np.minimum(residual_load, self.maximum_discharge_el())
+            energy_dch = np.minimum((power_dch / self.eta_out), (
+                self.__current_soc - 1 + self.dod_max) * self.capacity)
 
-        # in current_soc the time-changing variable of power is assigned
-        # (named energy)
-        self.__current_soc = (self.__current_soc - energy_dch /
-            self.__capacity)
+            # in current_soc the time-changing variable of power is assigned
+            # (named energy)
+            self.__current_soc = (
+                self.__current_soc - energy_dch / self.capacity)
 
-        return power_dch, self.__current_soc
+            return power_dch, self.__current_soc
 
     def charge(self, residual_load):
-        power_ch = np.minimum(residual_load, self.maximum_charge_el())
-        energy_ch = np.minimum((power_ch * self.eta_in), (1 - (
-            self.__current_soc)) * self.__capacity)
+        if self.capacity == 0:
+            return 0.0, 0.0
+        else:
+            power_ch = np.minimum(residual_load, self.maximum_charge_el())
+            energy_ch = np.minimum((power_ch * self.eta_in), (1 - (
+                self.__current_soc)) * self.capacity)
 
-        # in current_soc the time-changing variable of power is assigned
-        # (named energy)
-        self.__current_soc = (self.__current_soc + energy_ch /
-            self.__capacity)
+            # in current_soc the time-changing variable of power is assigned
+            # (named energy)
+            self.__current_soc = (
+                self.__current_soc + energy_ch / self.capacity)
 
-        return power_ch, self.__current_soc
+            return power_ch, self.__current_soc
+
+
+class PV(FixedSource):
+    def __init__(self, **kwargs):
+
+        super().__init__(**kwargs)
+
+        self.feedin = kwargs.get('feedin', None)
+
